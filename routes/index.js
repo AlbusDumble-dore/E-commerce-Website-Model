89aa7bdccd34bd15822b6fe4de6982c90c732ffd
Mukul Kumar <kumar.mukul377@gmail.com>
2017-08-01 07:01:53 +0530

    Interface editing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/products');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+  Product.find(function (err,docs) {
+
+    var productChunks = [];
+      var chunkSixe = 3;
+      for(i=0;i<docs.length;i+= chunkSixe)
+      {
+          productChunks.push(docs.slice(i,i + chunkSixe));
+      }
+      res.render('shop/index', { title: 'Shopping Cart',products: productChunks  });//   retreiving
+
+      
+  });
 });
 
 module.exports = router;
